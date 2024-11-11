d2194a66259caa88af763248937346fb4ee80e53
Jacob Parry <54328477+Jacob-Vance@users.noreply.github.com>
2024-11-10 21:24:01 -0600

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,14 +1,42 @@
 <?php
-// start session
 
-// login to the softball database
+session_start();
 
-// select password from users where username = <what the user typed in>
+include_once 'validate.php';
+$endUser = test_input($_POST['user']);
+$endUserPassword = test_input($_POST['pwd']);
 
-// if no rows, then username is not valid (but don't tell Mallory) just send
-// her back to the login
+if (strlen($endUser) < 1 || strlen($endUserPassword) < 1) {
+    header("location:index.php");
+    exit();
+}
 
-// otherwise, password_verify(password from form, password from db)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "softball";
 
-// if good, put username in session, otherwise send back to login
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
+$sql = "SELECT password FROM users WHERE username = '$endUser'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    if ($row = $result->fetch_assoc()) {
+        $verified = password_verify($endUserPassword, trim($row['password']));
+        if ($verified) {
+            $_SESSION['username'] = $endUser;
+            $_SESSION['error'] = '';
+        } else {
+            $_SESSION['error'] = 'invalid username or password';
+        }
+    }
+} else {
+    $_SESSION['error'] = 'invalid username or password';
+}
+$conn->close();
+header("location:index.php");
+?>
